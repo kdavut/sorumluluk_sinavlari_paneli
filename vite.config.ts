5e200d787db9eb7db74db3d57f8482dac20d46c2
Davut KILIÇ <93946530+kdavut@users.noreply.github.com>
2026-01-04 00:07:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // './' kullanımı, projenin hangi alt klasörde olduğundan bağımsız olarak dosyaları bulmasını sağlar
   base: './', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
