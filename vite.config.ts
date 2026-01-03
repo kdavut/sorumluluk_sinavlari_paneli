fa2db9b9b13b5c08568c68bb84c7d3d09941a95e
Davut KILIÇ <93946530+kdavut@users.noreply.github.com>
2026-01-03 23:56:53 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Boş string veya './' kullanımı asset yollarının doğru çözülmesini sağlar
-  base: '', 
+  // './' kullanımı, projenin hangi alt klasörde olduğundan bağımsız olarak dosyaları bulmasını sağlar
+  base: './', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
